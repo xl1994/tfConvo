e495a220453a4028fa55dd949c931bc279728de7
Lumos <champion556@163.com>
2017-09-22 18:55:17 +0800

    Update TFR.py

--- TFR.py
+++ TFR.py
@@ -1,17 +1,13 @@
 #  -*- Coding: UTF-8 -*-
-'''
-THIS IS THE PYTHON MODULE THAT IMPLEMENT BASIC FUNCTION OF CONVERTING PICTURE FILE TO TENDORFLOW TFRECORDS FILE AND
-READING AND DECODING TFRECORDS FILE
-THE PROGRAM WAS INITIALLY RUN ON A NONGRAPHIC CENTOS MACHINE
-'''
 import os 
 import tensorflow as tf 
 from PIL import Image 
-# import matplotlib.pyplot as plt 
 import numpy as np
 
 # turn off warning
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+global num_class
 
 number =['0','1','2','3','4','5','6','7','8','9']
 alphabet_lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -21,37 +17,29 @@ num_train_examples = 400*62
 num_valid_examples = 80*62
 img_width = 40
 img_height = 60
-img_pixels = img_width * img_height
 num_class = 62
 
-# In order to use gentfr() function, your picture should be stored in the directory under the current working directory
 def gentfr(cwd, classes, filename):
-    # cwd -> directory that stores your pictures
-    # classes -> all classes needed in the classification program
-    # filename -> file name of tfrecords file
     writer= tf.python_io.TFRecordWriter(filename)
     for index,name in enumerate(classes):
          class_path=cwd+'/'+name+'/'
-         # rewrite labels on 2017/09/22
-         # label should be column vector
-         label = np.zeros([62,1])
-         label[index] = 1
+         # label = np.zeros([num_class, 1])
+         # label[index] = 1
          for img_name in os.listdir(class_path): 
               img_path=class_path+img_name
               img=Image.open(img_path)
               # img= img.resize((128,128))
               img_raw=img.tobytes()
-              # modify the following line change int64_list=tf.train.Int64List(value=[index])) to int64_list=tf.train.Int64List(value=[label]))
-              example = tf.train.Example(features=tf.train.Features(feature={'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label])),'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))}))
+              example = tf.train.Example(features=tf.train.Features(feature={'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))}))
               writer.write(example.SerializeToString())
     writer.close()
     return filename
 
 def read_and_decode(filename, b_size):
-    global img_height
-    global img_width
-    global img_pixels
-    global class_num
+    img_height = 60
+    img_width = 40
+    img_pixels = img_width * img_height
+    class_num = 62
     # BATCH_SIZE = 100
     filename_queue = tf.train.string_input_producer([filename])
     reader = tf.TFRecordReader()
@@ -76,10 +64,10 @@ def test_records(filename):
          threads = tf.train.start_queue_runners(sess = sess)
          for i in range(5):
               val, label = sess.run([image_batch, label_batch])
+              label = tf.sparse_to_dense(label, [len(label), 64], 1)
               print val.shape, label.shape
 
-def fetchData(filename, b_size, mode=1):
-    # generate one-hot lobels
+def fetchData_v1(filename, b_size, mode=1):
     # mode = 1, parse label as column vectors
     # mode = 0, parse lable as row vectors
     image_batch, label_batch = read_and_decode(filename, b_size)
@@ -98,7 +86,17 @@ def fetchData(filename, b_size, mode=1):
               label_array[i,label[i]] = 1
     return img, label_array
   
-def one_hot_labels(labels):
+def fetch_data(filename, b_size, one_hot=True):
+    image_batch, label_batch = read_and_decode(filename, b_size)
+    global num_class
+    with tf.Session() as sess:
+         sess.run(tf.global_variables_initializer())
+         threads = tf.train.start_queue_runners(sess = sess)
+         img, label = sess.run([image_batch, label_batch])
+         if one_hot:
+              # label = tf.sparse_to_dense(label, [len(label), num_class], 1)
+              label = tf.one_hot(label, num_class, 1, 0)
+    return img, label
  
  
 if '__main__' == __name__:
@@ -110,9 +108,6 @@ if '__main__' == __name__:
     # train_file = gentfr(path,classes,'train.tfrecords')
     # valid_file = gentfr(path,classes,'valid.tfrecords')
 
-    img, label = fetchData('train.tfrecords',100)
-    print img.shape
-    print label.shape
-    for i in range(100):
-         print label[:,i]
-         print '\n'
+    img, label = fetch_data('train.tfrecords',100)
+    print label
+    # test_records('train.tfrecords')
